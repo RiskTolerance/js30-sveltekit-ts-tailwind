f9bae996a79b9e3e3a92e2e78f4fbb11fc3fbb66
Isaac Druin <isaac.druin@gmail.com>
2023-05-28 19:06:38 -0500

    I have no idea

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,7 @@
 			<li><a href="/">Home</a></li>
 			<li><a href="/1_Drum_Kit">Drum Kit</a></li>
 			<li><a href="/2_Clock">Clock</a></li>
+			<li><a href="/3_CSS_Variables">CSS Variables</a></li>
 		</ol>
 	</nav>
 	<slot />
---
+++ src/routes/3_CSS_Variables/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	let color: String = '#5e3fef';
+	let colStrength: Number = 0;
+	let blurVal: Number = 0;
+	let padding: Number | null = 0;
+</script>
+
+<div>
+	<h2>Hue Shift</h2>
+	<input type="color" bind:value={color} name="" id="" />
+	<h2>Color Strength</h2>
+	<input type="range" bind:value={colStrength} name="" id="" min="0" max="100" />
+	<h2>Blur</h2>
+	<input type="range" bind:value={blurVal} name="blur" min="0" max="100" />
+	<div>
+		<h2>Scale Border</h2>
+		<input type="range" bind:value={padding} min="0" max="50" />
+	</div>
+</div>
+
+<div
+	style="border-width: {padding}px; border-color: {color};"
+	class="relative w-[400px] h-[400px] border-solid"
+>
+	<div
+		style="background-color: {color};opacity: {colStrength}%; mix-blend-mode: hue;"
+		class="absolute w-full h-full z-20"
+	/>
+	<img
+		class="absolute w-full h-full z-10"
+		style="filter: blur({blurVal}px);"
+		src="/src/lib/images/clockbg.jpg"
+		alt=""
+	/>
+</div>
